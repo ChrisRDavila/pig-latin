38ef0c7984524507922ebe69fbb5da4ffc8d24d6
ChrisRDavila <crossdavila@gmail.com>
2023-03-29 16:42:06 -0700

    test for single letter vowel working

--- js/scrtips.js
+++ js/scrtips.js
@@ -2,18 +2,18 @@ function pigLatin(text) {
   let inputArray = text.split(" ");
   console.log(inputArray);
   const vowelArray = ["a", "e", "i", "o", "u"];
-  let index = 0;
   let outputArray = [];
   inputArray.forEach(element => {
     for (let i = 0; i < element.length; i++) {
-      if (vowelArray.includes(element.charAt(i).toLowerCase)) {
+      if (vowelArray.includes(element.charAt(i).toLowerCase())) {
         if(i === 0){
-          outputArray.push(element.slice(1, (element.length)) + element.slice(0, 1) + "way");
+          outputArray.push(element.slice(1, (element.length)) + element.slice(0, 1) + "way")
+          console.log(outputArray);
         }
       }
     }
-    return outputArray;
   })
+  return outputArray;
 }
 //if (i === vowelArray)
 //return  vowelArray + way
